334feabc80c29afee424b0a16736b24e19a875ef
Jonas Lampert <jonas.lampert.privat@gmail.com>
2023-01-31 17:34:27 +0100

    Projektdateien hinzufügen.

---
+++ Shop.DataModels/Migrations/20230130203210_fial.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Shop.DataModels.Migrations
+{
+    public partial class fial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AdminInfo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(6)", maxLength: 6, nullable: true),
+                    LastLogin = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: true),
+                    CreatedOn = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: true),
+                    UpdatedOn = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AdminInfo", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customer",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: true),
+                    MobileNo = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(6)", maxLength: 6, nullable: true),
+                    LastLogin = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: true),
+                    CreatedOn = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: true),
+                    UpdatedOn = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customer", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CustomerOrder",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerId = table.Column<int>(type: "int", nullable: true),
+                    OrderId = table.Column<string>(type: "nvarchar(9)", maxLength: 9, nullable: true),
+                    PaymentMode = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    ShippingAddress = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    ShippingCharges = table.Column<int>(type: "int", nullable: true),
+                    SubTotal = table.Column<int>(type: "int", nullable: true),
+                    Total = table.Column<int>(type: "int", nullable: true),
+                    ShippingStatus = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedOn = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: true),
+                    UpdatedOn = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerOrder", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderDetail",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderId = table.Column<string>(type: "nvarchar(9)", maxLength: 9, nullable: true),
+                    ProductId = table.Column<int>(type: "int", nullable: true),
+                    Quantity = table.Column<int>(type: "int", nullable: true),
+                    Price = table.Column<int>(type: "int", nullable: true),
+                    SubTotal = table.Column<int>(type: "int", nullable: true),
+                    CreatedOn = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: true),
+                    UpdatedOn = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderDetail", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: true),
+                    Price = table.Column<int>(type: "int", nullable: true),
+                    CategoryId = table.Column<int>(type: "int", nullable: true),
+                    ImageUrl = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: true),
+                    Stock = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ShippingStatus",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(200)", unicode: false, maxLength: 200, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShippingStatus", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AdminInfo");
+
+            migrationBuilder.DropTable(
+                name: "Category");
+
+            migrationBuilder.DropTable(
+                name: "Customer");
+
+            migrationBuilder.DropTable(
+                name: "CustomerOrder");
+
+            migrationBuilder.DropTable(
+                name: "OrderDetail");
+
+            migrationBuilder.DropTable(
+                name: "Product");
+
+            migrationBuilder.DropTable(
+                name: "ShippingStatus");
+        }
+    }
+}
---
+++ Shop.DataModels/Models/ShoppingDBContext.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Shop.DataModels.Models
+{
+    public partial class ShoppingDBContext : DbContext
+    {
+        public ShoppingDBContext()
+        {
+        }
+
+        public ShoppingDBContext(DbContextOptions<ShoppingDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<AdminInfo> AdminInfos { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<CustomerOrder> CustomerOrders { get; set; }
+        public virtual DbSet<OrderDetail> OrderDetails { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<ShippingStatus> ShippingStatuses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=ShoppingDB;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AdminInfo>(entity =>
+            {
+                entity.ToTable("AdminInfo");
+
+                entity.Property(e => e.CreatedOn).HasMaxLength(25);
+
+                entity.Property(e => e.Email).HasMaxLength(30);
+
+                entity.Property(e => e.LastLogin).HasMaxLength(25);
+
+                entity.Property(e => e.Name).HasMaxLength(100);
+
+                entity.Property(e => e.Password).HasMaxLength(6);
+
+                entity.Property(e => e.UpdatedOn).HasMaxLength(25);
+            });
+
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.ToTable("Category");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.ToTable("Customer");
+
+                entity.Property(e => e.CreatedOn).HasMaxLength(25);
+
+                entity.Property(e => e.Email).HasMaxLength(30);
+
+                entity.Property(e => e.LastLogin).HasMaxLength(25);
+
+                entity.Property(e => e.MobileNo).HasMaxLength(10);
+
+                entity.Property(e => e.Name).HasMaxLength(100);
+
+                entity.Property(e => e.Password).HasMaxLength(6);
+
+                entity.Property(e => e.UpdatedOn).HasMaxLength(25);
+            });
+
+            modelBuilder.Entity<CustomerOrder>(entity =>
+            {
+                entity.ToTable("CustomerOrder");
+
+                entity.Property(e => e.CreatedOn).HasMaxLength(25);
+
+                entity.Property(e => e.OrderId).HasMaxLength(9);
+
+                entity.Property(e => e.PaymentMode).HasMaxLength(50);
+
+                entity.Property(e => e.ShippingAddress).HasMaxLength(200);
+
+                entity.Property(e => e.UpdatedOn).HasMaxLength(25);
+            });
+
+            modelBuilder.Entity<OrderDetail>(entity =>
+            {
+                entity.ToTable("OrderDetail");
+
+                entity.Property(e => e.CreatedOn).HasMaxLength(25);
+
+                entity.Property(e => e.OrderId).HasMaxLength(9);
+
+                entity.Property(e => e.UpdatedOn).HasMaxLength(25);
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.ToTable("Product");
+
+                entity.Property(e => e.ImageUrl)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<ShippingStatus>(entity =>
+            {
+                entity.ToTable("ShippingStatus");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
